424c51eddca21e1217fba29ea2bfe5052adb9277
Riya-676 <riyavtechnocomet@gmail.com>
2026-02-04 21:50:50 +0530

    Add files via upload

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+$conn = mysqli_connect("localhost","root","","office_management");
+
+if(!$conn){
+ die("Database not connected");
+}
+?>
---
+++ office_management.sql
@@ -0,0 +1,94 @@
+-- Database create
+CREATE DATABASE office_management;
+USE office_management;
+
+-- =========================
+-- ADMIN TABLE
+-- =========================
+CREATE TABLE admin (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL
+);
+
+INSERT INTO admin (username, password)
+VALUES ('admin', 'admin123');
+
+-- =========================
+-- CLIENT TABLE
+-- =========================
+CREATE TABLE client (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    phone VARCHAR(15),
+    address VARCHAR(200)
+);
+
+INSERT INTO client (name, email, password, phone, address)
+VALUES 
+('Riya Vagadiya', 'riya@gmail.com', '12345', '9876543210', 'Rajkot');
+
+-- =========================
+-- TASK TABLE
+-- =========================
+CREATE TABLE task (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    client_id INT,
+    task_name VARCHAR(200),
+    task_description VARCHAR(300),
+    status VARCHAR(50) DEFAULT 'Pending',
+    FOREIGN KEY (client_id) REFERENCES client(id)
+);
+
+-- =========================
+-- ATTENDANCE TABLE
+-- =========================
+CREATE TABLE attendance (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    client_id INT,
+    date DATE,
+    status VARCHAR(20),
+    FOREIGN KEY (client_id) REFERENCES client(id)
+);
+
+-- =========================
+-- LEAVE REQUEST TABLE
+-- =========================
+CREATE TABLE leave_request (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    client_id INT,
+    reason VARCHAR(200),
+    leave_date DATE,
+    status VARCHAR(50) DEFAULT 'Pending',
+    FOREIGN KEY (client_id) REFERENCES client(id)
+);
+-- PROJECTS TABLE
+CREATE TABLE projects (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    project_name VARCHAR(100),
+    description TEXT,
+    start_date DATE,
+    end_date DATE,
+    status VARCHAR(50)
+);
+
+-- TASKS TABLE
+CREATE TABLE tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    project_id INT,
+    task_name VARCHAR(100),
+    assigned_to VARCHAR(50),
+    status VARCHAR(50),
+    due_date DATE
+);
+
+-- TIMESHEET TABLE
+CREATE TABLE timesheet (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    employee_name VARCHAR(50),
+    work_date DATE,
+    hours_worked INT,
+    work_description TEXT
+);
